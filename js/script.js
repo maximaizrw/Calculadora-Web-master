d88c49c391002fe90782434450ff5e7424ca4b58
Maximiliano Damian Maiz <maximaizrw@gmail.com>
2022-11-13 13:25:57 -0300

    Ajuste

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function calculadoraMP(){
     let precioSteam, precioSteamDolar, porcentajeGanancia = 0, precioVenta, clasica, ganancia, impuesto, impuesto2, uno, dos, tres, cuatro, cinco, suma;
-    let impuestoGanancia = 2; // 2 impuesto ganancia + 2 de posible IBB
-    let impuestoValorAgregado = 11;
     let porcentajeClasica = 2;
-    let ivaProductosNuevos = 5;
     let ibb = 3;
+    let unidad = 120;
 
     precioSteam = parseInt(document.getElementById('precioSteam').value);
 
@@ -13,45 +11,29 @@ function calculadoraMP(){
 
     //**********************************************************
         // Se calcula el porcentaje de la ganancia que espero
-        if (precioSteam <= 500) {
-            porcentajeGanancia = ((precioSteam * 23) / 100);
-        } else if (precioSteam >= 500 && precioSteam < 800) {
-            porcentajeGanancia = ((precioSteam * 20) / 100);
-        } else if (precioSteam >= 800 && precioSteam < 1999) {
-            porcentajeGanancia = ((precioSteam * 18) / 100);
-        } else if (precioSteam >= 2000) {
-            porcentajeGanancia = ((precioSteam * 15) / 100);
-        }
+
+            porcentajeGanancia = ((precioSteam * 25) / 100);
+
         //*********************************************************
 
         precioVenta = precioSteamDolar + porcentajeGanancia;
-        precioVenta += ((precioVenta * porcentajeClasica) / 100);
-        precioVenta += ((precioVenta * impuestoGanancia) / 100);
-        precioVenta += ((precioVenta * impuestoValorAgregado) / 100);
-        precioVenta += ((precioVenta * ivaProductosNuevos) / 100);
+        precioVenta += (((precioVenta * porcentajeClasica) / 100) + unidad); // Sumo publicacion clasica
         precioVenta += ((precioVenta * ibb) / 100);
         
-        uno = ((precioVenta * porcentajeClasica) / 100); //clasica
-        dos = ((precioVenta * 2) / 100); //impuesto
-        tres = ((precioVenta * 11) / 100); //impuesto2
-        cuatro = ((precioVenta * ivaProductosNuevos) / 100); //impuesto3
+        uno = ((precioVenta * porcentajeClasica) / 100) + unidad; //clasica
         cinco = ((precioVenta * ibb) / 100); //impuesto4
-        ganancia = precioVenta - uno - dos - tres - cuatro - cinco - precioSteamDolar;
+        ganancia = precioVenta - uno - cinco - precioSteamDolar;
         suma = porcentajeGanancia - ganancia;
         precioVenta += suma;
 
         for (let i = 0; i < 40; i++) {	// REPITO HASTA TENER LA GANANCIA ESPERADA
             if (suma != 0) {
 
-                uno = ((precioVenta * porcentajeClasica) / 100); //clasica
-                dos = ((precioVenta * impuestoGanancia) / 100); //impuesto
-                tres = ((precioVenta * impuestoValorAgregado) / 100); //impuesto2
-                cuatro = ((precioVenta * ivaProductosNuevos) / 100); //impuesto3
+                uno = ((precioVenta * porcentajeClasica) / 100) + unidad; //clasica
                 cinco = ((precioVenta * ibb) / 100); //impuesto4
-                ganancia = precioVenta - uno - dos - tres - cuatro - cinco - precioSteamDolar;
+                ganancia = precioVenta - uno - cinco - precioSteamDolar;
                 suma = porcentajeGanancia - ganancia;
                 precioVenta += suma;
-
             }
         }
 
@@ -69,15 +51,9 @@ function calculadoraT(){
 
     //**********************************************************
         // Se calcula el porcentaje de la ganancia que espero
-        if (precioSteam <= 500) {
-            porcentajeGanancia = ((precioSteam * 23) / 100);
-        } else if (precioSteam >= 500 && precioSteam < 800) {
-            porcentajeGanancia = ((precioSteam * 20) / 100);
-        } else if (precioSteam >= 800 && precioSteam < 1999) {
-            porcentajeGanancia = ((precioSteam * 18) / 100);
-        } else if (precioSteam >= 2000) {
-            porcentajeGanancia = ((precioSteam * 15) / 100);
-        }
+
+            porcentajeGanancia = ((precioSteam * 25) / 100);
+
         //*********************************************************
 
         precioVenta = precioSteamDolar + porcentajeGanancia;
@@ -99,7 +75,7 @@ function calculadoraT(){
     document.getElementById('resultadoT').innerHTML = Math.round(precioVenta);
 }
 
-function calculadoraML(){
+/*function calculadoraML(){
     let precioSteam, precioSteamDolar, porcentajeGanancia = 0, precioVenta, ganancia, uno, dos, tres, cuatro, cinco, suma;
     let unidad = 60;
     let impuestoGanancia = 2;
@@ -151,6 +127,48 @@ function calculadoraML(){
                 cuatro = ((precioVenta * ivaProductosNuevos) / 100); //impuesto3
                 cinco = ((precioVenta * ibb) / 100); //impuesto4
                 ganancia = precioVenta - uno - dos - tres - cuatro - cinco - precioSteamDolar;
+                suma = porcentajeGanancia - ganancia;
+                precioVenta += suma;
+            }
+        }
+
+    document.getElementById('resultadoML').innerHTML = Math.round(precioVenta);
+}*/
+
+function calculadoraML(){
+    let precioSteam, precioSteamDolar, porcentajeGanancia = 0, precioVenta, ganancia, uno, dos, tres, cuatro, cinco, suma;
+    let unidad = 120;
+    let porcentajeClasica = 11;
+    let ibb = 3;
+
+    precioSteam = parseInt(document.getElementById('precioSteam').value);
+
+    // Se calcula el precio con el impuesto pais + dolar 100%
+    precioSteamDolar = precioSteam +((precioSteam*100)/100);
+
+    //**********************************************************
+        // Se calcula el porcentaje de la ganancia que espero
+
+            porcentajeGanancia = ((precioSteamDolar * 25) / 100);
+       
+        //*********************************************************
+
+        precioVenta = precioSteamDolar + porcentajeGanancia;
+        precioVenta += (((precioVenta * porcentajeClasica) / 100) + unidad); // Sumo publicacion clasica
+        precioVenta += ((precioVenta * ibb) / 100);
+
+        uno = ((precioVenta * porcentajeClasica) / 100) + unidad; //clasica
+        cinco = ((precioVenta * ibb) / 100); //impuesto4
+        ganancia = precioVenta - uno - cinco - precioSteamDolar;
+        suma = porcentajeGanancia - ganancia;
+        precioVenta += suma;
+
+        for (let i = 0; i < 40; i++) {	// REPITO HASTA TENER LA GANANCIA ESPERADA
+            if (suma != 0) {
+
+                uno = ((precioVenta * porcentajeClasica) / 100) + unidad; //clasica
+                cinco = ((precioVenta * ibb) / 100); //impuesto4
+                ganancia = precioVenta - uno - cinco - precioSteamDolar;
                 suma = porcentajeGanancia - ganancia;
                 precioVenta += suma;
             }
