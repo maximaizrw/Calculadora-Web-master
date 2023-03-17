26f440343575434ddef9669abe6acf2a557db37f
Maximiliano Damian Maiz <maximaizrw@gmail.com>
2023-03-17 00:08:10 -0300

    ..

--- js/calculos.js
+++ js/calculos.js
@@ -1,7 +1,7 @@
 //Definicion de constantes:
 const IBB = 3;
 const UNIDAD = 150; //Precio al vender UNIDAD.
-const PORCENTAJE_CLASICA_MP = 0.03;
+const PORCENTAJE_CLASICA_MP = 0.08;
 const PORCENTAJE_CLASICA_ML = 0.13;
 const PORCENTAJE_GANANCIA_ESPERADA = 0.45;
 const PORCENTAJE_IMPUESTO_DOLAR = 0.75;
